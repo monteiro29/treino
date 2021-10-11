39904a8270ad48e6fa811428fda9fd8ba7bcdd06
andremonteiropartner <andre.monteiro@partner.bmw.de>
2021-10-11 11:51:58 +0100

    code

--- exemplo_monteiro.py
+++ exemplo_monteiro.py
@@ -1,1 +1,39 @@
-asdasdqweqweW
+
+#print('ola')
+
+a = 'string'
+b = 1
+c = '1111'
+d = []
+e = [1, 'o', 'pp']
+f = {'key': 90, 'b': 80}
+g = (1, 1)
+
+#if 0 == b:
+#    print(e)
+#if 1 == b:
+#    print(g)
+#    print(e)
+
+#if '1' == c:
+#    print(c)
+#elif '11' == c:
+#    print(d)
+#else:
+#    print('ups')
+
+#for i in e:
+#    print(i)
+    
+#for i in range(1,6):
+#    print(i)
+    
+#for k,v in f.items():
+#    print(k,v)
+
+
+print(e)
+e.append('lll«')
+print(e)
+
+
